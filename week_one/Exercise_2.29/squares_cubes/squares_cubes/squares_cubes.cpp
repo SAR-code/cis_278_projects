0e36885a5011191396f4a92f671ee35f25343bdc
SAR-code <dylanmccallum28@gmail.com>
2025-01-21 20:03:20 -0700

    assignment 2 complete

---
+++ week_one/Exercise_2.29/squares_cubes/squares_cubes/squares_cubes.cpp
@@ -0,0 +1,34 @@
+/*
+* script: squares_cubes.cpp
+* action: This program calculates the squares and cubes of the integers
+*         from 0 to 10.
+* author: D.M
+* date: 21JAN25
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // creating a table of integers, squares, and cubes using only escape sequences and chapter 2 techniques
+    cout << "Integer\tSquare\tCube\n";
+    cout << 0 << "\t" << 0 * 0 << "\t" << 0 * 0 * 0 << "\n";
+    cout << 1 << "\t" << 1 * 1 << "\t" << 1 * 1 * 1 << "\n";
+    cout << 2 << "\t" << 2 * 2 << "\t" << 2 * 2 * 2 << "\n";
+    cout << 3 << "\t" << 3 * 3 << "\t" << 3 * 3 * 3 << "\n";
+    cout << 4 << "\t" << 4 * 4 << "\t" << 4 * 4 * 4 << "\n";
+    cout << 5 << "\t" << 5 * 5 << "\t" << 5 * 5 * 5 << "\n";
+    cout << 6 << "\t" << 6 * 6 << "\t" << 6 * 6 * 6 << "\n";
+    cout << 7 << "\t" << 7 * 7 << "\t" << 7 * 7 * 7 << "\n";
+    cout << 8 << "\t" << 8 * 8 << "\t" << 8 * 8 * 8 << "\n";
+    cout << 9 << "\t" << 9 * 9 << "\t" << 9 * 9 * 9 << "\n";
+    cout << 10 << "\t" << 10 * 10 << "\t" << 10 * 10 * 10 << "\n";
+
+    return 0;
+}
+
+
+
+
